0b256c1236baa74ca2546d26acf23e2b3e0c4682
Quan Vu <nongdenchet8435848@gmail.com>
2016-11-26 09:34:05 +0700

    refactor

--- app/src/main/java/apidez/com/week8/activity/RegisterActivity.java
+++ app/src/main/java/apidez/com/week8/activity/RegisterActivity.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 import apidez.com.week8.MyApplication;
 import apidez.com.week8.R;
 import apidez.com.week8.databinding.ActivityRegisterBinding;
+import apidez.com.week8.dependency.component.UserComponent;
 import apidez.com.week8.dependency.module.UserModule;
 import apidez.com.week8.viewmodel.RegisterViewModel;
 import butterknife.ButterKnife;
@@ -19,6 +20,7 @@ import rx.schedulers.Schedulers;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
+    private UserComponent userComponent;
 
     @Inject
     RegisterViewModel viewModel;
@@ -26,9 +28,10 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((MyApplication) getApplication()).component()
-                .plus(new UserModule())
-                .inject(this);
+        userComponent = ((MyApplication) getApplication())
+                .component()
+                .plus(new UserModule());
+        userComponent.inject(this);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_register);
         binding.setViewModel(viewModel);
         ButterKnife.bind(this);
@@ -48,6 +51,8 @@ public class RegisterActivity extends AppCompatActivity {
         viewModel.register()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(value -> {}, throwable -> {});
+                .subscribe(value -> {
+                }, throwable -> {
+                });
     }
 }
